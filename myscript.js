ff645106e8af2dfa7fc1f7305de7c8d4d24d58a2
Zamanof <zamanov@itstep.edu.az>
2023-09-13 12:29:11 +0400

    JS1

---
+++ myscript.js
@@ -0,0 +1,96 @@
+// console.log("Hello Step")
+// console.log("Hello Javascript")
+// console.log(3*30)
+// console.log(document)
+
+// console.log("Hi")
+
+// var name = 'Nadir'
+// var Name = 'Salam'
+//
+// console.log(name)
+// console.log(Name)
+
+// var variable = 25
+// console.log(typeof (variable))
+// Data types
+/*
+1. Undefined - bir dəyər qəbul edir undefined
+
+2. Null - bir dəyər qəbul edir null
+
+3. Boolean - true, false
+
+4. String - sətir ("str",  'Salam')
+
+5. Number - ədədi tip  Number.MIN_VALUE =  5e-324   -  Number.MAX_VALUE = 1.79e+308
+    - NaN - not a number
+    - Infinity
+    - -Infinity
+    - -0
+
+6. Object
+
+7. Symbol
+*/
+
+
+
+/*
+Riyazi operatorlar
++
+-
+*
+/
+%
+++
+--
++=
+-=
+*=
+/=
+%=
+*/
+
+// console.log('Salam' + "Saqol")
+// console.log('1' + "2")
+// console.log('1' + 2)
+// console.log('3' / '12')
+// console.log(typeof (+'2'))
+
+number = '256'
+
+// console.log(typeof(Number.parseInt(number)))
+// console.log(typeof(Number(number)))
+// console.log(typeof(+number))
+
+/*
+Condition operators
+>
+>=
+<
+<=
+==
+!=
+===
+!==
+*/
+
+// var val1 = 3
+// var val2 = '3'
+//
+// console.log(val1 === val2)
+
+/*
+Logic operators:
+&&
+||
+!
+
+bitwise operators:
+&
+|
+^
+<<
+>>
+*/
